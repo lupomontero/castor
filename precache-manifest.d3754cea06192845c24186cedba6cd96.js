427161593272230eb905adef256b3c80cedfb10b
Lupo Montero <lupomontero@gmail.com>
2018-12-28 12:09:30 -0500

    Updates

---
+++ precache-manifest.d3754cea06192845c24186cedba6cd96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "403e8b4eb8c6cefca144",
+    "url": "/static/css/main.9ef6d07f.chunk.css"
+  },
+  {
+    "revision": "403e8b4eb8c6cefca144",
+    "url": "/static/js/main.403e8b4e.chunk.js"
+  },
+  {
+    "revision": "d6a53880fa30d0574792",
+    "url": "/static/js/1.d6a53880.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0772cbf970ff9493cfc44a8f2b941ce",
+    "url": "/index.html"
+  }
+];
